5a573000093c6995e5ca30092357572326285d14
sakitayu <sakitakitakita@gmail.com>
2020-06-23 00:45:20 +0900

    relationshipコントローラーを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :relationships, only: [:create, :destroy]
 end
