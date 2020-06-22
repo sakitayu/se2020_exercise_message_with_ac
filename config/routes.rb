af5da3bea97ff6daa6210fa6742644a5ba96ba29
sakitayu <sakitakitakita@gmail.com>
2020-06-23 01:05:12 +0900

    ルートを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show]
   resources :relationships, only: [:create, :destroy]
 end
