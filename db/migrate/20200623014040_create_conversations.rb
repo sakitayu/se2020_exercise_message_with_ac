4693bf013eed709f2aa22a94a80a18c5797bd753
sakitayu <sakitakitakita@gmail.com>
2020-06-23 10:48:07 +0900

    Messageモデルを生成

--- db/migrate/20200623014040_create_conversations.rb
+++ db/migrate/20200623014040_create_conversations.rb
@@ -3,8 +3,10 @@ class CreateConversations < ActiveRecord::Migration[5.2]
     create_table :conversations do |t|
       t.integer :sender_id
       t.integer :recipient_id
-
       t.timestamps
     end
+    add_index :conversations, :sender_id
+    add_index :conversations, :recipient_id
+    add_index :conversations, [:sender_id, :recipient_id], unique: true
   end
 end
