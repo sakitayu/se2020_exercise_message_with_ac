786ef3f310ac9cf1efa8ee57b271eaf702dcf99b
sakitayu <sakitakitakita@gmail.com>
2020-07-02 06:21:12 +0900

    画像投稿機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  mount_uploader :image, ImageUploader
 
   #指定のユーザをフォローする
   def follow!(other_user)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'rooms/show'
   root to: 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
 
   resources :conversations do
