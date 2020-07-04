6ee1960ad9eb256472e741d8041d4081637a30cf
sakitayu <sakitakitakita@gmail.com>
2020-07-05 07:31:00 +0900

    プロフィールに画像を表示するように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path, notice: "プロフィールを編集しました！"
+      redirect_to users_path(@user.id), notice: "プロフィールを編集しました！"
     else
       render :edit
     end
